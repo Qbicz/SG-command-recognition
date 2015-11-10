9f016dee0c61ce824a409902923d88f148fc9bb2
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-11-10 17:51:18 +0100

    Further filtering + a new sound sample
    The sample now has a loud background noise.

--- 1_preprocessing/preprocess.py
+++ 1_preprocessing/preprocess.py
@@ -1,54 +1,85 @@
 import math
 import numpy as np
 import pylab as plt
-from scipy.fftpack import fft, ifft, fftfreq
+from scipy.fftpack import fft, fftfreq
 from scipy.io import wavfile
-from scipy.signal import butter, filtfilt, lfilter
+from scipy.signal import butter, filtfilt, freqs
 
-filename = 'probka2.wav'
-filename_out = 'probka2_filtered.wav'
+filename = 'probka1.wav'
+filename_out = 'probka1_filtered.wav'
 
 [fs, frames] = wavfile.read(filename)
 ts = 1 / fs
 nyquist_f = fs / 2
 
-# signal = (frames[:, 0] + frames[:, 1]) / 2
+# signal = (frames[:, 0] + frames[:, 1]) / 2  # mean value of both channels
 signal = frames[:, 0]  # left (?) channel
 time = np.arange(0, len(signal) * ts, ts)
 
-# we can use Hz in here when the filter is analog
-low = 100
-high = 10000
+# signal = signal[(time < 0.5)]
+# time = time[(time < 0.5)]
+
+# values in fraction of nyquist_f
+low = 0.1
+high = 0.20
 
 # actual filter
-# b, a = butter(2, (low, high), 'bandpass', analog=True)
-b, a = butter(2, (0.01, 0.2), 'bandpass')
-bandpass = filtfilt(b, a, signal)
-# bandpass = lfilter(b, a, signal)
-bandpass = bandpass.astype('int16')
+b, a = butter(2, (low, high), 'bandpass')
+w, h = freqs(b, a)  # frequency response of the filter
+bandpass = filtfilt(b, a, signal)  # apply filter to the signal
+bandpass *= 2  # GAIN SOME POWER
+bandpass = bandpass.astype('int16')  # to save the results in correct format
 
 # save the results
 wavfile.write(filename_out, fs, bandpass)
 
 # Fourier transform of the original signal
-freqs = fftfreq(signal.size, ts)
-signal_f = fft(signal)
+frequencies = fftfreq(signal.size, ts)
+frequencies = frequencies[range(int(len(frequencies) / 2))]
+signal_f = fft(signal) / len(signal)
+signal_f = signal_f[range(int(len(signal_f) / 2))]  # only for positive f.
+# signal_f = signal_f[(frequencies < 1000)]
+# frequencies = frequencies[(frequencies < 1000)]
 
 # Fourier of the bandpassed
 freqs_band = fftfreq(bandpass.size, ts)
-bandpass_f = fft(bandpass)
+freqs_band = freqs_band[range(int(len(freqs_band) / 2))]
+bandpass_f = fft(bandpass) / len(bandpass)
+bandpass_f = bandpass_f[range(int(len(bandpass_f) / 2))]
+# bandpass_f = bandpass_f[(freqs_band < 1000)]
+# freqs_band = freqs_band[(freqs_band < 1000)]
 
-plt.subplot(221)
+plt.subplot(2, 3, 1)
 plt.plot(time, signal)
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude')
+plt.grid()
 
-plt.subplot(222)
-plt.plot(freqs, signal_f)
+plt.subplot(2, 3, 4)
+plt.plot(frequencies, np.abs(signal_f), 'r')
+plt.xlabel('Freq [Hz]')
+plt.ylabel('|Y(freq)|')
+plt.grid()
 
-plt.subplot(223)
+plt.subplot(2, 3, 2)
 plt.plot(time, bandpass)
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude')
+plt.grid()
 
-plt.subplot(224)
-plt.plot(freqs_band, bandpass_f)
+plt.subplot(2, 3, 5)
+plt.plot(freqs_band, np.abs(bandpass_f), 'r')
+plt.xlabel('Freq [Hz]')
+plt.ylabel('|Y(freq)|')
+plt.grid()
+
+plt.subplot(2, 3, 3)
+plt.semilogx(w, 20 * np.log10(abs(h)))
+plt.title('Butterworth filter frequency response')
+plt.xlabel('Freq [rad/s]')
+plt.ylabel('Amplitude [dB]')
+plt.margins(0, 0.1)
+plt.grid(which='both', axis='both')
 
 plt.show()
 
